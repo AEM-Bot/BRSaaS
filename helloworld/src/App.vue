e2f24da439b0bb94a9cf51948ac665824ee1b396
schintam <schintam@victoria.com>
2024-08-09 14:35:56 -0400

    Basic Dropdown Feature

--- helloworld/src/App.vue
+++ helloworld/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
+  <!--  <img alt="Vue logo" src="./assets/logo.png">-->
   <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
@@ -15,6 +15,16 @@ export default {
 </script>
 
 <style>
+.hello {
+  text-align: center;
+  font-weight: 900;
+  font-size: 300%;
+  margin-left: 519px;
+  min-height: 120px;
+  width: 800px;
+  padding-top: 8%;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
